9206bc040be7436a09bc5d2e5fa6c35053f94bd7
Ryan <reyaznyc@gmail.com>
2022-11-24 23:47:03 -0500

    deploy auth server

--- authServer.js
+++ authServer.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 const express = require('express')
 const app = express()
-const PORT = 4001
+const PORT = process.env.PORT || 4001
 
 const cors = require('cors')
 const mysql = require('mysql')
