43fc7781fac1e144eabd73a1d787ffb3a26c9f5a
Ryan <reyaznyc@gmail.com>
2022-11-25 00:35:24 -0500

    first deployment

--- authServer.js
+++ authServer.js
@@ -8,10 +8,10 @@ const cors = require('cors')
 const mysql = require('mysql')
 
 app.use(express.json())
-app.use(cors({
-    credentials: true,
-    origin: 'http://localhost:3000'
-}))
+// app.use(cors({
+//     credentials: true,
+//     origin: 'http://localhost:3000'
+// }))
 
 //establish routers - middleware
 const authRouter = require('./routes/auth')
